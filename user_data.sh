8085c51762ece71203e4386ece171c6ad3a708fe
Jin Park <forjin@vpac-innovations.com.au>
2016-08-31 10:54:47 +1000

    aws user data script for authorize user from AWS S3

---
+++ user_data.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cat << EOF > /etc/ssh/keys.sh
+#!/bin/bash
+
+public_key_file="landblade/authorized_keys"
+bucket="vpac-innovations-keys"
+s3Key="<<AWS_S3_KEY>>"
+s3Secret="<<AWS_S3_SECRET>>"
+contentType="plain/text"
+
+dateValue=\`date -R\`
+resource="/\${bucket}/\${public_key_file}"
+stringToSign="GET\n\n\${contentType}\n\${dateValue}\n\${resource}"
+signature=\`echo -en \${stringToSign} | openssl sha1 -hmac \${s3Secret} -binary | base64\`
+
+curl \
+     -H "Host: \${bucket}.s3.amazonaws.com" \
+     -H "Date: \${dateValue}" \
+     -H "Content-Type: \${contentType}" \
+     -H "Authorization: AWS \${s3Key}:\${signature}" \
+     https://\${bucket}.s3.amazonaws.com/\${public_key_file}
+
+EOF
+
+chmod +x /etc/ssh/keys.sh
+if grep -q AuthorizedKeysCommand "/etc/ssh/sshd_config"; then
+    echo "AuthorizedKeysCommand found"
+else
+    echo "AuthorizedKeysCommand /etc/ssh/keys.sh" >> /etc/ssh/sshd_config
+    echo "AuthorizedKeysCommandUser root" >> /etc/ssh/sshd_config
+fi
+service ssh restart
